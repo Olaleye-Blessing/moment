89a79b20fa464fef16af58c100c47b4512cffc39
Olaleye Blessing <Olayinkablexxy@gmail.com>
2021-07-06 20:51:15 +0100

    configure react route in express

--- server/app.js
+++ server/app.js
@@ -18,17 +18,21 @@ app.use(express.json({ limit: "30mb" }));
 
 app.use(cookieParser());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-    // app.use(
-    //     "/static",
-    //     express.static(path.join(__dirname, "../client/build//static"))
-    // );
-}
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("client/build"));
+//     // app.use(
+//     //     "/static",
+//     //     express.static(path.join(__dirname, "../client/build//static"))
+//     // );
+// }
 
 app.use("/moments", momentRoutes);
 app.use("/auth", authenticationRoutes);
 app.use("/comments", commentRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+});
 
 app.use(globalErrorHandler);
 export { app };
